2153563e7e4b30a5f2891df8d295c173c36256ec
Abhimanyu Tyagi <abhimanyutyagi2@gmail.com>
2018-02-11 20:12:59 -0500

    Added additional next waypoint logic in C++ to replicate what is being done in blueprint

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,15 +2,30 @@
 
 #include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"					//TODO Remove coupling
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	
+	// Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
 
-	// UE_LOG(LogTemp, Warning, TEXT("AI in C++"));
+	// Set the next waypoint
 	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	BlackboardComponent->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// TODO protect against empty patrol routes
+
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComponent->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	// UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
